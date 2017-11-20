b93f6279e3df0bf69a9740d47b0614a3ab7a413b
HollowsxD <hollowsxd@users.noreply.github.com>
2017-11-20 23:30:54 +0800

    Create asufo.js
    Add main code

---
+++ asufo.js
@@ -0,0 +1,4 @@
+for(y=0; y<=23; y++){
+    var questionNo = document.getElementsByName('data[AnalyticSufo][answer'+y+']');
+        questionNo[2].checked = true
+	document.getElementsByClassName('btn btn-success btn-flat')[0].click()
